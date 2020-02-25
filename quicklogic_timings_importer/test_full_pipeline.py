a2185a4b67fdf67b58510524217b88b00a438f8a
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-02-25 18:39:03 +0100

    Cleaned test_full_pipeline
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- quicklogic_timings_importer/test_full_pipeline.py
+++ quicklogic_timings_importer/test_full_pipeline.py
@@ -23,7 +23,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
             "inputlist",
-            help="File containing list of absolute " + \
+            help="File containing list of absolute " +
                  "paths to test Liberty files",
             type=Path)
     parser.add_argument(
@@ -40,7 +40,8 @@ def main():
             action='store_true')
     parser.add_argument(
             "--lib-similarity-threshold",
-            help="Float value representing similarity threshold below which files are no longer considered to be similar",
+            help="Float value representing similarity threshold below " +
+            "which files are no longer considered to be similar",
             type=similarity_float,
             default=0.999,
             )
@@ -56,7 +57,7 @@ def main():
     log_printer.SUPPRESSBELOW = args.log_suppress_below
 
     with open(args.inputlist, 'r') as liblistfile:
-       liblist = [Path(path.strip()) for path in liblistfile.readlines()]
+        liblist = [Path(path.strip()) for path in liblistfile.readlines()]
 
     liblist = sorted(liblist)
 
@@ -77,10 +78,12 @@ def main():
     filenum = 0
     numfiles = len(liblist)
 
+    procline = '[{:04d}/{:04d},lj={:04d},jn={:04d},nj={:04d},jj={:04d},ll={:04d}] Processing {}'  # noqa: E501
+
     for libname in liblist:
         filenum += 1
         namecore = Path(str(libname.parent)[1:]) / libname.stem
-        print('[{:04d}/{:04d},lj={:04d},jn={:04d},nj={:04d},jj={:04d},ll={:04d}] Processing {}'.format(
+        print(procline.format(
             filenum,
             numfiles,
             numfailed['lib-to-json'],
@@ -95,15 +98,20 @@ def main():
         jsondict = {}
         # try parsing LIB file
         try:
-            jsondict = LibertyToJSONParser.load_timing_info_from_lib(inputliberty)
+            jsondict = LibertyToJSONParser.load_timing_info_from_lib(
+                    inputliberty)
             if args.output_json_root_dir:
-                targetfile = Path(str(args.output_json_root_dir / namecore) + '.json')
+                targetfile = Path(
+                        str(args.output_json_root_dir / namecore) + '.json')
                 targetfile.parent.mkdir(parents=True, exist_ok=True)
                 with open(targetfile, 'w') as out:
                     json.dump(jsondict, out, indent=2)
             log_printer.log('INFO', ' lib-to-json: {}'.format(libname))
         except Exception as ex:
-            log_printer.log('ERROR', 'lib-to-json:  {} | {}'.format(type(ex).__name__, libname))
+            log_printer.log(
+                    'ERROR', 'lib-to-json:  {} | {}'.format(
+                        type(ex).__name__,
+                        libname))
             numfailed['lib-to-json'] += 1
             numskipped['json-to-lib'] += 1
             numskipped['newlib-to-json'] += 1
@@ -115,13 +123,16 @@ def main():
         try:
             liblines = JSONToLibertyWriter.convert_json_to_liberty(jsondict)
             if args.output_lib_root_dir:
-                targetfile = Path(str(args.output_lib_root_dir / namecore) + '.lib')
+                targetfile = Path(
+                        str(args.output_lib_root_dir / namecore) + '.lib')
                 targetfile.parent.mkdir(parents=True, exist_ok=True)
                 with open(targetfile, 'w') as out:
                     out.write('\n'.join(liblines))
             log_printer.log('INFO', ' json-to-lib: {}'.format(libname))
         except Exception as ex:
-            log_printer.log('ERROR', 'json-to-lib:  {} | {}'.format(type(ex).__name__, libname))
+            log_printer.log(
+                    'ERROR', 'json-to-lib:  {} | {}'.format(
+                        type(ex).__name__, libname))
             numfailed['json-to-lib'] += 1
             numskipped['newlib-to-json'] += 1
             numskipped['comparison-json'] += 1
@@ -133,13 +144,16 @@ def main():
         try:
             newjson = LibertyToJSONParser.load_timing_info_from_lib(liblines)
             if args.output_json_root_dir:
-                targetfile = Path(str(args.output_json_root_dir / namecore) + '-new.json')
+                targetfile = Path(
+                        str(args.output_json_root_dir / namecore) +
+                        '-new.json')
                 targetfile.parent.mkdir(parents=True, exist_ok=True)
                 with open(targetfile, 'w') as out:
                     json.dump(newjson, out, indent=2)
             log_printer.log('INFO', ' newlib-to-json: {}'.format(libname))
         except Exception as ex:
-            log_printer.log('ERROR', 'newlib-to-json:  {} | {}'.format(type(ex).__name__, libname))
+            log_printer.log('ERROR', 'newlib-to-json:  {} | {}'.format(
+                type(ex).__name__, libname))
             numfailed['newlib-to-json'] += 1
             numskipped['comparison-json'] += 1
             numskipped['comparison-lib'] += 1
@@ -147,25 +161,36 @@ def main():
         if jsondict == newjson:
             log_printer.log('INFO', ' comparison-json: {}'.format(libname))
         else:
-            log_printer.log('ERROR', 'comparison-json:  {} | {}'.format('jsondict != newjson', libname))
+            log_printer.log('ERROR', 'comparison-json:  {} | {}'.format(
+                'jsondict != newjson', libname))
             with open('{}_wrong.json'.format(filenum), 'w') as wrong:
                 json.dump(newjson, wrong, indent=2)
             numfailed['comparison-json'] += 1
         in1 = clean_lines(inputliberty[1:-1])
         in2 = clean_lines(liblines[1:-1])
-        similarity = diff_files(in1, in2, print_diff=False, return_similarity=True, similarity_method='quick')
+        similarity = diff_files(
+                in1,
+                in2,
+                print_diff=False,
+                return_similarity=True,
+                similarity_method='quick')
         if similarity > args.lib_similarity_threshold:
-            log_printer.log('INFO', ' comparison-lib: {} | {}'.format(similarity, libname))
+            log_printer.log('INFO', ' comparison-lib: {} | {}'.format(
+                similarity, libname))
         else:
-            log_printer.log('ERROR', 'comparison-lib:  {} | {}'.format(similarity, libname))
+            log_printer.log('ERROR', 'comparison-lib:  {} | {}'.format(
+                similarity, libname))
             numfailed['comparison-lib'] += 1
             if args.print_lib_diff:
                 diff_files(in1, in2, print_diff=True)
 
-
     for key in numfailed.keys():
-        print('{}:  {} out of {} failed ({}% succeded, out of which {} were skipped)'.format(
-            key, numfailed[key], numfiles, 100.0 * (numfiles - numfailed[key]) / numfiles, numskipped[key]))
+        print('{}: {} out of {} failed ({}% succeded, {} were skipped)'.format(
+            key,
+            numfailed[key],
+            numfiles,
+            100.0 * (numfiles - numfailed[key]) / numfiles, numskipped[key]))
+
 
 if __name__ == '__main__':
     main()
